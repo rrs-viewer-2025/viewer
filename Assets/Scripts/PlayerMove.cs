c81732ed2e57da77117a6335ebe636abc3e6604f
mi-1227 <140976341+mi-1227@users.noreply.github.com>
2025-04-26 00:26:35 +0900

    Joy-ConController

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -12,6 +12,10 @@ public class PlayerCharaControl : MonoBehaviour
     private Animator anim;
     private bool runFlag;
     Rigidbody rb;
+    private List<Joycon> joycons; // Joy-Conのリスト
+    private Joycon joycon;        // 今使うJoy-Con（片方）
+    private float h;
+    private float v;
 
 
     // Start is called before the first frame update
@@ -23,13 +27,35 @@ public class PlayerCharaControl : MonoBehaviour
         // リセットボタンの設定
         Button resetButton = GameObject.Find("ResetButton").GetComponent<Button>();
         resetButton.onClick.AddListener(ResetSimulation); // リセットボタンをクリックしたときにResetSimulationを呼び出す
+        
+         // JoyconManager から Joy-Con のリストを取得
+        joycons = JoyconManager.Instance.j;
+
+        // 少なくとも1つJoy-Conが接続されていたら使う
+        if (joycons.Count > 0)
+        {
+            joycon = joycons[0]; // 0番目のJoy-Conを使用（通常は左）
+            //Debug.Log("Joy-Con接続成功");
+        }
+        else
+        {
+            //Debug.Log("Joy-Conが接続されていません");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        float v = Input.GetAxis("Vertical"); //上下キーの取得
-        float h = Input.GetAxis("Horizontal"); //左右キーの取得
+    if(joycon != null){
+        var stick = joycon.GetStick();
+        //Debug.Log("Joy-Con Input:" + stick[0] + stick[1]);  // 入力値を確認
+        v = -stick[0];
+        h = stick[1];
+    }
+    else{
+        v = Input.GetAxis("Vertical"); //上下キーの取得
+        h = Input.GetAxis("Horizontal"); //左右キーの取得
+        }
 
         if(v>0.1||v<-0.1||h>0.1||h<-0.1){
             runFlag = true;
